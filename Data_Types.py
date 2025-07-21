ac8029ea8fc4c2c33201b9cef4202df0120e2a73
Abdullah Qureshi <fa24-bcs-300@students.cuisahiwal.edu.pk>
2025-07-21 18:01:22 +0500

    Update Data_Types.py

--- Data_Types.py
+++ Data_Types.py
@@ -36,49 +36,49 @@ print(l,type(l))
 ####All Data Types in Python
 
 # Numeric Types
-int_var = 42              # int
-float_var = 3.14          # float
-complex_var = 2 + 3j      # complex
+ahmed = 42                     # int
+bilal = 3.14                   # float
+kashif = 2 + 3j                # complex
 
 # Text Type
-str_var = "Hello, Python" # str
+faizan = "Hello, Python"       # str
 
 # Sequence Types
-list_var = [1, 2, 3]           # list
-tuple_var = (1, 2, 3)          # tuple
-range_var = range(5)          # range
+zahid_list = [1, 2, 3]              # list
+yasir_tuple = (1, 2, 3)             # tuple
+hamza_range = range(5)             # range
 
 # Mapping Type
-dict_var = {"name": "Alice", "age": 30}  # dict
+usman_dict = {"name": "Usman", "age": 30}  # dict
 
 # Set Types
-set_var = {1, 2, 3}           # set
-frozenset_var = frozenset([1, 2, 3])  # frozenset
+imran_set = {1, 2, 3}                  # set
+noman_frozenset = frozenset([1, 2, 3])  # frozenset
 
 # Boolean Type
-bool_var = True              # bool
+saad = True                           # bool
 
 # Binary Types
-bytes_var = b"hello"         # bytes
-bytearray_var = bytearray(5) # bytearray
-memoryview_var = memoryview(bytes_var) # memoryview
+tariq_bytes = b"hello"                # bytes
+adil_bytearray = bytearray(5)         # bytearray
+nabeel_memoryview = memoryview(tariq_bytes)  # memoryview
 
 # None Type
-none_var = None              # NoneType
+junaid = None                         # NoneType
 
 # Print all types
-print(type(int_var))
-print(type(float_var))
-print(type(complex_var))
-print(type(str_var))
-print(type(list_var))
-print(type(tuple_var))
-print(type(range_var))
-print(type(dict_var))
-print(type(set_var))
-print(type(frozenset_var))
-print(type(bool_var))
-print(type(bytes_var))
-print(type(bytearray_var))
-print(type(memoryview_var))
-print(type(none_var))
+print(type(ahmed))
+print(type(bilal))
+print(type(kashif))
+print(type(faizan))
+print(type(zahid_list))
+print(type(yasir_tuple))
+print(type(hamza_range))
+print(type(usman_dict))
+print(type(imran_set))
+print(type(noman_frozenset))
+print(type(saad))
+print(type(tariq_bytes))
+print(type(adil_bytearray))
+print(type(nabeel_memoryview))
+print(type(junaid))
