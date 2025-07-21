e652c33577fcbcb3c97997da5bd1ff10734e2013
Abdullah Qureshi <fa24-bcs-300@students.cuisahiwal.edu.pk>
2025-07-21 17:44:20 +0500

    Create First.py
    Start python from Zero.

---
+++ First.py
@@ -0,0 +1,32 @@
+print("Hello World, Welcome to NxGen Solution" )
+print(20+50)
+print(10>5)
+
+#multiple lines
+print("Hello, World!")
+print("Welcome to learning Python.")
+print("This is fun!")
+
+#Using variable
+
+message = "Hello, World!"
+name = "Home"
+print(message)
+print("Welcome,", name)
+
+
+#User input
+
+name = input("Enter your name: ")
+print("Hello,", name)
+
+
+#  => add 2 numbr
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# strings to integers
+sum = int(num1) + int(num2)
+
+print("The sum is:", sum)
+
