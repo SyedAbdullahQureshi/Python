9d93e351f12a571ae43ef8900484c6c4657690f4
Abdullah Qureshi <fa24-bcs-300@students.cuisahiwal.edu.pk>
2025-07-21 17:44:57 +0500

    Create Variable_Check.py

---
+++ Variable_Check.py
@@ -0,0 +1,46 @@
+a=10
+b=20
+print(a+b)
+print('a',b)
+print(id(a),id(b))
+
+c="hello"
+print(c)
+
+a1=200
+print(a1)
+
+a_b=35
+print('a_b')
+
+_a=20
+print(_a)
+
+
+#check type of variable
+
+name = "bilu"
+age = 20
+
+print(type(name))     # string
+print(type(age))      # intiger
+
+
+
+
+first_name = "Umer"
+last_name = "Watto"
+age = 30
+name1 = "Bilu"  
+
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Age:", age)
+print("Name 1:", name1)
+
+# Check type and location
+print("Type of age:", type(age))
+print("Memory ID of age:", id(age))
+print("Type of name1:", type(name1))
+print("Memory ID of name1:", id(name1))
