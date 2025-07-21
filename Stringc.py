547fce58d7f80f69bfbfd60818f2104ef6c6747b
Abdullah Qureshi <fa24-bcs-300@students.cuisahiwal.edu.pk>
2025-07-21 17:46:17 +0500

    Create Stringc.py

---
+++ Stringc.py
@@ -0,0 +1,19 @@
+a="Hello"
+b="NxGen Sloution"
+
+print(a+" "+b)
+c=30
+print(c+30)
+print(a+a)
+
+
+# String and numbr mix code
+
+name = "Bilu"
+age = 25
+
+# Using f-string
+print(f"{name} is {age} years old.")
+
+# Using + (convert number to string)
+print(name + " is " + str(age) + " years old.")
