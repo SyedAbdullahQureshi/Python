8a49d513b447f748f52a3d18412c14bf74038680
Abdullah Qureshi <fa24-bcs-300@students.cuisahiwal.edu.pk>
2025-07-21 17:48:01 +0500

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,130 @@
+a="Hello"
+b="NxGen Sloution"
+
+print(a+" "+b)
+c=30
+print(c+30)
+print(a+a)
+
+
+# String and numbr mix code
+
+name = "Bilu"
+age = 25
+
+# Using f-string
+print(f"{name} is {age} years old.")
+
+# Using + (convert number to string)
+print(name + " is " + str(age) + " years old.")
+
+
+a=10
+b=20
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+#modulus
+print(a/b)
+print(a%3)
+
+
+
+#exponent
+
+print(5**2)
+print(5**3)
+
+#flow division
+print(10//3)
+print(16//3)
+
+
+#all opreator use below
+
+
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+
+# Add
+print("Addition: a + b =", a + b)
+
+# Subtract
+print("Subtraction: a - b =", a - b)
+
+# Multiply
+print("Multiplication: a * b =", a * b)
+
+# Divide
+if b != 0:
+    print("Division: a / b =", a / b)
+else:
+    print("DivideByZero")
+
+# Modulus
+if b != 0:
+    print("Modulus: a % b =", a % b)
+else:
+    print("DivideByZero")
+
+# Power
+print("Exponent: a ** b =", a ** b)
+
+# FloorDiv
+if b != 0:
+    print("Floor Division: a // b =", a // b)
+else:
+    print("DivideByZero")
+
+
+#use of Assignment Operators
+
+x=5
+print(x)
+
+x=x+5
+print(x)
+
+x+=5
+print(x)
+
+x=x-5
+print(x)
+
+x-=5
+print(x)
+
+x-=5
+print(x)
+
+
+
+
+#Use of assignment operator in given below code
+
+
+a = int(input("Enter a number: "))
+
+# Add
+a += 5
+print("After += 5:", a)
+
+# Subtract
+a -= 3
+print("After -= 3:", a)
+
+# Multiply
+a *= 2
+print("After *= 2:", a)
+
+# Divide
+a /= 4
+print("After /= 4:", a)
+
+# Modulus
+a %= 2
+print("After %= 3:", a)
+
+
